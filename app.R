621b72d7d06ed4c55b304fe802945d59499e1c93
James Blair <james.m.blair09@gmail.com>
2020-06-11 13:34:02 -0600

    Synchronous shiny app

---
+++ app.R
@@ -0,0 +1,50 @@
+library(shiny)
+library(DBI)
+library(pool)
+
+con <- dbPool(RSQLite::SQLite(), dbname = "tmp.sqlite")
+
+onStop(function() {
+  poolClose(pool)
+})
+
+shinyApp(
+  ui = fluidPage(
+    textOutput("time"),
+    actionButton("btn", "Long query!"),
+    textOutput("query_status"),
+    sliderInput("cyls", "Cylinders", min = 1, max = 8, value = 4, step = 1),
+    plotOutput("plt")
+  ),
+  
+  server = function(input, output, session) {
+    output$time <- renderText({
+      invalidateLater(1000)
+      paste0("Time: ", format(Sys.time()))
+    })
+    
+    query_result <- eventReactive(input$btn, {
+      DBI::dbGetQuery(con,
+                      "WITH RECURSIVE r(i) AS (
+                          VALUES(0)
+                          UNION ALL
+                          SELECT i FROM r
+                          LIMIT 100000000
+                        )
+                        SELECT i FROM r WHERE i = 1;")
+    })
+    
+    output$query_status <- renderText({
+      query_result() %>%
+        (function(result) {
+          paste0("[", Sys.time(), "] Query completed: ", result)
+        })
+    })
+    
+    output$plt <- renderPlot({
+      plot(mpg ~ disp, data = mtcars[mtcars$cyl >= input$cyls,,drop = FALSE],
+           pch = 16, cex = 2, col = cyl)
+    })
+  }
+)
+
