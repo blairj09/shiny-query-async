5dd421f6dc3ba8ace27a2f42d502e41c377f78af
James Blair <james.m.blair09@gmail.com>
2020-06-15 17:23:11 -0600

    Add async app

---
+++ R/async/app.R
@@ -0,0 +1,56 @@
+library(shiny)
+library(promises)
+library(future)
+library(DBI)
+
+plan(multisession)
+
+shinyApp(
+  ui = fluidPage(
+    textOutput("time"),
+    actionButton("btn", "Long query!"),
+    textOutput("query_status"),
+    sliderInput("cyls", "Cylinders", min = 1, max = 8, value = 4, step = 1),
+    plotOutput("plt")
+  ),
+  
+  server = function(input, output, session) {
+    output$time <- renderText({
+      invalidateLater(1000)
+      paste0("Time: ", format(Sys.time()))
+    })
+    
+    query_result <- eventReactive(input$btn, {
+      future({
+        x <- system.time({
+          # The DB connection needs to be created and destroyed within the future
+          con <- DBI::dbConnect(RSQLite::SQLite(), "tmp.sqlite")
+          DBI::dbGetQuery(con,
+                          "WITH RECURSIVE r(i) AS (
+                          VALUES(0)
+                          UNION ALL
+                          SELECT i FROM r
+                          LIMIT 10000000
+                        )
+                        SELECT i FROM r WHERE i = 1;")
+          DBI::dbDisconnect(con)
+        })
+        
+        x[["elapsed"]]
+      })
+      
+    })
+    
+    output$query_status <- renderText({
+      query_result() %...>%
+        (function(result) {
+          paste0("[", Sys.time(), "] Query completed in ", result, " seconds")
+        })
+    })
+    
+    output$plt <- renderPlot({
+      plot(mpg ~ disp, data = mtcars[mtcars$cyl >= input$cyls,,drop = FALSE],
+           pch = 16, cex = 2, col = cyl)
+    })
+  }
+)
