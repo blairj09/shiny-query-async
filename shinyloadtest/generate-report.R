87aa2d8028450ebfa39d0aecdd9f94b9580b64b0
James Blair <james.m.blair09@gmail.com>
2020-06-30 19:38:22 -0600

    Loadtests for async2

--- shinyloadtest/generate-report.R
+++ shinyloadtest/generate-report.R
@@ -2,6 +2,7 @@ library(shinyloadtest)
 
 df <- load_runs("sync" = here::here("shinyloadtest", "runs", "sync"),
                 "sync scaled" = here::here("shinyloadtest", "runs", "sync-scaled"),
-                "async" = here::here("shinyloadtest", "runs", "async"))
+                "async" = here::here("shinyloadtest", "runs", "async"),
+                "async2" = here::here("shinyloadtest", "runs", "async2"))
 
 shinyloadtest_report(df, output = here::here("shinyloadtest", "report.html"))
--- shinyloadtest/run-loadtests.sh
+++ shinyloadtest/run-loadtests.sh
@@ -3,3 +3,4 @@
 shinycannon recording.log https://rst-precise-grubworm.soleng.rstudioservices.com/rsconnect/sync --workers 20 --loaded-duration-minutes 20 --output-dir runs/sync               
 shinycannon recording.log https://rst-precise-grubworm.soleng.rstudioservices.com/rsconnect/sync-scaled --workers 20 --loaded-duration-minutes 20 --output-dir runs/sync-scaled
 shinycannon recording.log https://rst-precise-grubworm.soleng.rstudioservices.com/rsconnect/async --workers 20 --loaded-duration-minutes 20 --output-dir runs/async
+shinycannon recording.log https://rst-precise-grubworm.soleng.rstudioservices.com/rsconnect/async2 --workers 20 --loaded-duration-minutes 20 --output-dir runs/async2
